df42be3b63cba781520cc98e74a970eeed672282
muinnzz <konohagakuredes@gmail.com>
2026-03-28 15:10:23 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.post("/order", (req, res) => {
+  const { name, phone } = req.body;
+
+  const message = `Order dari ${name}, Phone: ${phone}`;
+  const whatsappURL = `https://wa.me/60166173129?text=${encodeURIComponent(message)}`;
+
+  res.redirect(whatsappURL);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server jalan bro"));
